0a8b97d6e012f9f82e8323d58f8c85fb32e66a5c
Antonio Emilio <antonio_pereira@outlook.com>
2026-02-18 16:41:15 +0100

    feat: fixing missing 10

--- project_galaxy/antonio/src/Model/Model_CPU/Model_CPU_fast/Model_CPU_fast.cpp
+++ project_galaxy/antonio/src/Model/Model_CPU/Model_CPU_fast/Model_CPU_fast.cpp
@@ -23,36 +23,6 @@ void Model_CPU_fast
 	std::memset(accelerationsy.data(), 0, n_particles * sizeof(float));
 	std::memset(accelerationsz.data(), 0, n_particles * sizeof(float));
 
-    /*#pragma omp parallel for
-    for (int i = 0; i < n_particles; i++)
-	{
-		for (int j = 0; j < n_particles; j++)
-		{
-			if(i != j)
-			{
-				const float diffx = particles.x[j] - particles.x[i];
-				const float diffy = particles.y[j] - particles.y[i];
-				const float diffz = particles.z[j] - particles.z[i];
-
-				float dij = diffx * diffx + diffy * diffy + diffz * diffz;
-
-				if (dij < 1.0)
-				{
-					dij = 10.0;
-				}
-				else
-				{
-					dij = std::sqrt(dij);
-					dij = 10.0 / (dij * dij * dij);
-				}
-
-				accelerationsx[i] += diffx * dij * initstate.masses[j];
-				accelerationsy[i] += diffy * dij * initstate.masses[j];
-				accelerationsz[i] += diffz * dij * initstate.masses[j];
-			}
-		}
-	}*/
-
 	#pragma omp parallel for
 	for (int i = 0; i < n_particles; i += b_type::size)
 	{
@@ -74,7 +44,7 @@ void Model_CPU_fast
 			b_type rdij = rdiffx*rdiffx + rdiffy*rdiffy + rdiffz*rdiffz;
 			
 			// SIMD version of the if-else statement
-			rdij = xs::select(rdij < 1.0f, b_type(10.0f), xs::sqrt(rdij) / (rdij * rdij));
+			rdij = xs::select(rdij < 1.0f, b_type(10.0f), b_type(10.0f) * xs::sqrt(rdij) / (rdij * rdij));
 			
 			b_type mass_factor = b_type(initstate.masses[j]);
 			raccx_i += rdiffx * rdij * mass_factor;
@@ -86,17 +56,6 @@ void Model_CPU_fast
 		raccy_i.store_unaligned(&accelerationsy[i]);
 		raccz_i.store_unaligned(&accelerationsz[i]);
 	}
-
-    /* #pragma omp parallel for
-	for (int i = 0; i < n_particles; i++)
-	{
-		velocitiesx[i] += accelerationsx[i] * 2.0f;
-		velocitiesy[i] += accelerationsy[i] * 2.0f;
-		velocitiesz[i] += accelerationsz[i] * 2.0f;
-		particles.x[i] += velocitiesx   [i] * 0.1f;
-		particles.y[i] += velocitiesy   [i] * 0.1f;
-		particles.z[i] += velocitiesz   [i] * 0.1f;
-	} */
 
 	#pragma omp parallel for
 	for (int i = 0; i < n_particles; i += b_type::size)
@@ -129,28 +88,6 @@ void Model_CPU_fast
 		py.store_unaligned(&particles.y[i]);
 		pz.store_unaligned(&particles.z[i]);
 	}
-
-// OMP  version
-// #pragma omp parallel for
-//     for (int i = 0; i < n_particles; i ++)
-//     {
-//     }
-
-
-// OMP + xsimd version
-// #pragma omp parallel for
-//     for (int i = 0; i < n_particles; i += b_type::size)
-//     {
-//         // load registers body i
-//         const b_type rposx_i = b_type::load_unaligned(&particles.x[i]);
-//         const b_type rposy_i = b_type::load_unaligned(&particles.y[i]);
-//         const b_type rposz_i = b_type::load_unaligned(&particles.z[i]);
-//               b_type raccx_i = b_type::load_unaligned(&accelerationsx[i]);
-//               b_type raccy_i = b_type::load_unaligned(&accelerationsy[i]);
-//               b_type raccz_i = b_type::load_unaligned(&accelerationsz[i]);
-
-//         ...
-//     }
 
 }
 
