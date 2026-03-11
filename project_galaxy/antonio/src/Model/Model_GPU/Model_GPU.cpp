de0ab02d4f209ddc66b243a9e5e93c4be281223d
melyssamariana <melyssa.mariana32@gmail.com>
2026-03-11 10:05:46 +0100

    a

--- project_galaxy/antonio/src/Model/Model_GPU/Model_GPU.cpp
+++ project_galaxy/antonio/src/Model/Model_GPU/Model_GPU.cpp
@@ -6,7 +6,6 @@
 #include "Model_GPU.hpp"
 #include "kernel.cuh"
 
-
 inline bool cuda_malloc(void ** devPtr, size_t size)
 {
 	cudaError_t cudaStatus;
@@ -40,7 +39,21 @@ void update_position_gpu(float3* positionsGPU, float3* velocitiesGPU, float3* ac
 		std::cout << "error: unable to synchronize threads" << std::endl;
 }
 
+/*
+n_particles comes from Model.hpp
+model_cpu:
+	velocitiesx   (n_particles),
+	accelerationsx(n_particles),
 
+	->  particles.x[i] = initstate.positionsx[i];
+		Initialize using the data from the Initstate object
+	->  std::copy(initstate.velocitiesx.begin(), initstate.velocitiesx.end(), velocitiesx.begin());
+		std::copy(1,2,3) - copy elements from one range to another. 
+       	1 and 2 specify the range of elements to copy (the source), 
+    	3 specifies the beginning of the destination range where the elements will be copied to
+		need to be copy to avoid modify Initstate
+		Starts with the initial velocities, but later it will modify them.
+	*/		
 Model_GPU
 ::Model_GPU(const Initstate& initstate, Particles& particles)
 : Model(initstate, particles),
@@ -68,6 +81,7 @@ Model_GPU
 		accelerationsf3[i].z = 0;
 	}
 
+	
 	cuda_malloc((void**)&positionsGPU,     n_particles * sizeof(float3));
 	cuda_malloc((void**)&velocitiesGPU,    n_particles * sizeof(float3));
 	cuda_malloc((void**)&accelerationsGPU, n_particles * sizeof(float3));
@@ -77,7 +91,6 @@ Model_GPU
 	cuda_memcpy(velocitiesGPU,    velocitiesf3.data()    , n_particles * sizeof(float3), cudaMemcpyHostToDevice);
 	cuda_memcpy(accelerationsGPU, accelerationsf3.data(), n_particles * sizeof(float3), cudaMemcpyHostToDevice);
 	cuda_memcpy(massesGPU,        initstate.masses.data(), n_particles * sizeof(float), cudaMemcpyHostToDevice);
-
 }
 
 Model_GPU
@@ -92,14 +105,39 @@ Model_GPU
 void Model_GPU
 ::step()
 {
+	/*
+	cpu_fast: 
+	->  std::fill(accelerationsx.begin(), accelerationsx.end(), 0);
+		std::fill(start_iterator, end_iterator, value);
+		set all elements of a container to the same value.
+
+	-> for bellow is the same, but was two fors
+		->	const float diffx = particles.x[j] - particles.x[i];
+			First was calculed the distance between particles in the positions i,j 
+		->  float dij = diffx * diffx + diffy * diffy + diffz * diffz;
+			than squared distance between the particles.
+			is used because it avoids computing a slow square root initially
+		->  if (dij < 1.0) dij = 10.0;
+			Handle very small distances (softening)
+			if distance  becomes very small → force becomes infinite → simulation explodes.
+			code limits the force when particles are too close.
+		->  else dij = std::sqrt(dij); dij = 10.0 / (dij * dij * dij);
+			apply equation gravitational acceleration
+		->  accelerationsx[i] += diffx * dij * initstate.masses[j];
+			adds the acceleration caused by particle j to particle i.
+	*/
 	update_position_gpu(positionsGPU, velocitiesGPU, accelerationsGPU, massesGPU, n_particles);
+	
 	cuda_memcpy(positionsf3.data(), positionsGPU, n_particles * sizeof(float3), cudaMemcpyDeviceToHost);
+
+	
 	for (int i = 0; i < n_particles; i++)
 	{
 		particles.x[i] = positionsf3[i].x;
 		particles.y[i] = positionsf3[i].y;
 		particles.z[i] = positionsf3[i].z;
 	}
+	
 }
 
 #endif // GALAX_MODEL_GPU
--- project_galaxy/antonio/src/Model/Model_GPU/kernel.cu
+++ project_galaxy/antonio/src/Model/Model_GPU/kernel.cu
@@ -3,26 +3,35 @@
 #include "cuda.h"
 #include "kernel.cuh"
 #define DIFF_T (0.1f)
-#define EPS (2.0f)
-
-__global__ void compute_acc(float3 * positionsGPU, float3 * velocitiesGPU, float3 * accelerationsGPU, float* massesGPU, int n_particles)
+#define EPS (1.0f)
+__global__ void compute_acc(
+	float3 * positionsGPU, 
+	float3 * velocitiesGPU, 
+	float3 * accelerationsGPU, 
+	float* massesGPU,
+	int n_particles)
 {
 	unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
 
-	if (i >= n_particles)
-	{
+	if (i >= n_particles) {
 		return;
 	}
 	
 	float ax = 0.0f, ay = 0.0f, az = 0.0f;
 
+	// Now each thread access positionsGPU[j] sequentially
+	// So:
+	float xi = positionsGPU[i].x;
+	float yi = positionsGPU[i].y;
+	float zi = positionsGPU[i].z;
+
 	for (int j = 0; j < n_particles; j++)
 	{
 		if(i != j)
 		{
-			const float diffx = positionsGPU[j].x - positionsGPU[i].x;
-			const float diffy = positionsGPU[j].y - positionsGPU[i].y;
-			const float diffz = positionsGPU[j].z - positionsGPU[i].z;
+			const float diffx = positionsGPU[j].x - xi;
+			const float diffy = positionsGPU[j].y - yi;
+			const float diffz = positionsGPU[j].z - zi;
 
 			float dij = diffx * diffx + diffy * diffy + diffz * diffz;
 
@@ -76,5 +85,26 @@ void update_position_cu(float3* positionsGPU, float3* velocitiesGPU, float3* acc
 	maj_pos    <<<nblocks, nthreads>>>(positionsGPU, velocitiesGPU, accelerationsGPU, n_particles);
 }
 
+/*
+__global__ void compute_acc(float3 * positionsGPU, float3 * velocitiesGPU, float3 * accelerationsGPU, float* massesGPU, int n_particles)
+{
+	unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+}
+
+__global__ void maj_pos(float3 * positionsGPU, float3 * velocitiesGPU, float3 * accelerationsGPU, int n_particles)
+{
+	unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+}
+
+void update_position_cu(float3* positionsGPU, float3* velocitiesGPU, float3* accelerationsGPU, float* massesGPU, int n_particles)
+{
+	int nthreads = 128;
+	int nblocks =  (n_particles + (nthreads -1)) / nthreads;
+
+	compute_acc<<<nblocks, nthreads>>>(positionsGPU, velocitiesGPU, accelerationsGPU, massesGPU, n_particles);
+	maj_pos    <<<nblocks, nthreads>>>(positionsGPU, velocitiesGPU, accelerationsGPU, n_particles);
+}
+*/
 
 #endif // GALAX_MODEL_GPU
